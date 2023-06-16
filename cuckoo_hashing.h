fa6f4ce0cf7e9d285e58db5ea6d30828aa8f5c37
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-06-16 13:13:10 -0300

    Update cuckoo_hashing.h

--- cuckoo_hashing.h
+++ cuckoo_hashing.h
@@ -1,1 +1,28 @@
+#ifndef cuckoo_hashing
+#define cuckoo_hashing
 
+#define TAM 22
+
+struct nodo{
+    int chave;
+    int excluido;
+    int null;
+};
+
+double floor(double x);
+
+void inicializa_hash(struct nodo hash[TAM]);
+
+int busca(int chave, struct nodo hash[TAM]);
+
+int h1(int chave);
+
+int h2(int chave);
+
+void insere_chave(int chave, struct nodo hash[TAM]);
+
+void remove_chave(int chave, struct nodo hash[TAM]);
+
+void imprime_hash(struct nodo hash[TAM]);
+
+#endif /* cuckoo_hashing.h*/
